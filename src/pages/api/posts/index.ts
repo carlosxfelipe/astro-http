47722e9950262a54b62997f137f1aa14ff52bff1
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-12-26 18:48:57 -0300

    Add API route to retrieve individual blog post by slug

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,11 +1,35 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 // http://localhost:4321/api/posts
+// http://localhost:4321/api/posts?slug=first-post
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
+
+  if (slug) {
+    const post = await getEntry("blog", slug);
+
+    if (post) {
+      return new Response(JSON.stringify(post), {
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+    }
+
+    return new Response(JSON.stringify({ msg: `Post ${slug} not found` }), {
+      status: 404,
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
+
   const posts = await getCollection("blog");
 
   return new Response(JSON.stringify(posts), {
