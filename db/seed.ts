cc59a0c153fdfddaae2fa062e5a13310e4a80a29
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2024-12-31 14:25:41 -0300

    Add Clients table definition and seed data for initial entries

--- db/seed.ts
+++ db/seed.ts
@@ -1,8 +1,12 @@
-import { db } from "astro:db";
+import { Clients, db } from "astro:db";
 
 // https://astro.build/db/seed
 export default async function seed() {
-  // TODO
+  await db.insert(Clients).values([
+    { id: 1, name: "Carlos Felipe", age: 37, isActive: true },
+    { id: 2, name: "Maria Isabelle", age: 33, isActive: true },
+    { id: 3, name: "Luís Felipe", age: 6, isActive: false },
+  ]);
 
   console.log("Seeded database");
 }
