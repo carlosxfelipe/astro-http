6dde348733123641de3d2f49931b7bbcc6ea4f33
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-07 13:52:16 -0300

    Add API routes for client operations (GET, POST, PATCH, DELETE)

---
+++ src/pages/api/clients/[clientId].ts
@@ -0,0 +1,46 @@
+import type { APIRoute } from "astro";
+
+export const prerender = false;
+
+export const GET: APIRoute = async ({ params, request }) => {
+  const clientId = params.clientId;
+
+  const body = {
+    method: "GET",
+    clientId,
+  };
+
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export const PATCH: APIRoute = async ({ params, request }) => {
+  const clientId = params.clientId;
+
+  const body = {
+    method: "PATCH",
+    clientId,
+  };
+
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
+
+export const DELETE: APIRoute = async ({ params, request }) => {
+  const clientId = params.clientId;
+
+  const body = {
+    method: "DELETE",
+    // clientId: +clientId!,
+    clientId,
+  };
+
+  return new Response(JSON.stringify(body), {
+    status: 200,
+    headers: { "Content-Type": "application/json" },
+  });
+};
