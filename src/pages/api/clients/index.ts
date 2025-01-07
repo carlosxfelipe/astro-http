99df1e772f986fa097a530f3df4ebc5af52e78ca
Carlos Felipe Araujo <carlosxfelipe@gmail.com>
2025-01-07 15:11:10 -0300

    Enhance API client routes to return user data and handle POST requests with error management

--- src/pages/api/clients/index.ts
+++ src/pages/api/clients/index.ts
@@ -1,25 +1,34 @@
 import type { APIRoute } from "astro";
+import { Clients, db } from "astro:db";
 
 export const prerender = false;
 
 export const GET: APIRoute = async ({ params, request }) => {
-  const body = {
-    method: "GET",
-  };
+  const users = await db.select().from(Clients);
 
-  return new Response(JSON.stringify(body), {
+  return new Response(JSON.stringify(users), {
     status: 200,
     headers: { "Content-Type": "application/json" },
   });
 };
 
 export const POST: APIRoute = async ({ params, request }) => {
-  const body = {
-    method: "POST",
-  };
+  try {
+    const { id, ...body } = await request.json();
+    const { lastInsertRowid } = await db.insert(Clients).values(body);
 
-  return new Response(JSON.stringify(body), {
-    status: 201,
-    headers: { "Content-Type": "application/json" },
-  });
+    return new Response(
+      JSON.stringify({ id: +lastInsertRowid!.toString(), ...body }),
+      {
+        status: 201,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
+  } catch (error) {
+    console.log(error);
+    return new Response(JSON.stringify({ msg: "body not found" }), {
+      status: 201,
+      headers: { "Content-Type": "application/json" },
+    });
+  }
 };
